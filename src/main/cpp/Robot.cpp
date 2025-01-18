3da3b26a60a58440b50f4fc49b52eee9958980ee
Camden Pastyrnak <camdenpastyrnak@gmail.com>
2025-01-18 13:53:46 -0500

    Initial Commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "Robot.h"
+#include <frc/smartdashboard/SmartDashboard.h>
+
+Robot::Robot() {
+  redux::canand::EnsureCANLinkServer();
+}
+void Robot::RobotPeriodic() {
+  frc::SmartDashboard::PutNumber("Gyro Yaw", ((units::degree_t) gyro.GetYaw()).value());
+}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic() {}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+void Robot::SimulationInit() {}
+void Robot::SimulationPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
